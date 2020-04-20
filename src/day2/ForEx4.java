106cc02a33ad9f64fb7916f8e14e8e082691c8d3
bb <bumji_@naver.com>
2020-04-20 17:43:48 +0900

    2일차 for문

--- src/day2/ForEx4.java
+++ src/day2/ForEx4.java
@@ -17,10 +17,10 @@ public class ForEx4 {
 		for(i=1; i<=num ; i++) {
 			if(num % i == 0)
 				
-			System.out.println(i);		
+			System.out.println(i);		//System.out.printf(""+i+(i!=num?",":""));
 					
 		}	
-		
+
 	}
 
 }
