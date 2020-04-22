bca18d088d94bfd4c434a4ba86c7e0dc90b8adca
bb <bumji_@naver.com>
2020-04-22 15:44:17 +0900

    3일차 For문 예제

---
+++ src/day3/ForEx2.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class ForEx2 {
+
+	public static void main(String[] args) {
+		/* 정수 num가 소수인지 아닌지 판별하는 코드를 for문을 이용하여 작성하세요.
+		 * 소수: 약수가 2개인 수
+		 *
+		 *  
+		 *  
+		 *  int i, num = 6;
+		
+		for(i=1; i<=num ; i++) {
+			if(num % i == 0)
+				
+			System.out.println(i);	
+		 *  
+		 *  
+		 *  
+		 */
+		
+		
+		int num=0;
+		Scanner scan = new Scanner(System.in);
+		num = Scanner.nextInt();
+		
+
+	}
+
+}
