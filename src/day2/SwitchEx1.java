3b79fdaad22161f2abd5a9a784c724df3199d50a
bb <bumji_@naver.com>
2020-04-24 17:48:22 +0900

    2일차 스위치문

--- src/day2/SwitchEx1.java
+++ src/day2/SwitchEx1.java
@@ -1,11 +1,15 @@
 package day2;
 
+import java.util.Scanner;
+
 public class SwitchEx1 {
 
 	public static void main(String[] args) {
 		// switch문을 이용한 홀짝 판별 예제
-		int num = 6;
-		switch(num % 2 ) {
+		//int num = 6;
+		Scanner scan = new Scanner(System.in); //
+		int num = scan.nextInt();
+		switch(num % 2 ) {    //num을 2로 나누었을 때 나머지를 구하는 
 		case 0:
 			System.out.println(num + "는 짝수");
 			break;
@@ -14,6 +18,9 @@ public class SwitchEx1 {
 			break;
 			
 		}
+
+		
+		
 		
 	}
 
